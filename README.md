440018cca830327d8a9eeb75c989de30b63a93f0
heqinhui_93@163.com <heimo.he@tophant.com>
2018-04-08 14:16:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# webIm
+
+> webIm based on PHP Swoole
+
+> 基于php swoole的 webIM聊天室
+
+- **环境搭建**
+
+`pecl install swoole`
+
+- **使用方法**
+
+`sudo php websocket_server.php`
+
+配置Nginx使用户能够访问到chat.html
---
+++ websocket_server.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heimo
+ * Date: 2017/6/8
+ * Time: 下午10:52
+ */
+
+/*
+创建wss
+
+$ws = new swoole_websocket_server("0.0.0.0", 9502, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+$ws->set(array(
+    'ssl_cert_file' => '/ssl/xxxx.crt',
+    'ssl_key_file' => '/ssl/xxxx.key',
+));
+*/
+
+
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new swoole_websocket_server("0.0.0.0", 9501);
+
+//监听WebSocket连接打开事件
+$ws->on('open', function ($ws, $request) {
+    //var_dump($ws);
+    //var_dump($request);
+    echo "client-{$request->fd} is connected\n";
+});
+
+//监听WebSocket消息事件
+$ws->on('message', function ($ws, $frame) {
+   foreach($ws->connections as $fd)
+    {
+        $ws->push($fd, $frame->data);
+    } 
+});
+
+//监听WebSocket连接关闭事件
+$ws->on('close', function ($ws, $fd) {
+    echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
